d0dbf50d0a322e06ca8b04a596742f3a033e48d5
wh <wh2523117255@gmail.com>
2023-03-15 08:05:50 +0800

    finish home

--- main.js
+++ main.js
@@ -23,3 +23,29 @@ export function createApp() {
   }
 }
 // #endif
+  
+uni.$showToast = function(title="数据加载出错!",duration=1500){
+   uni.showToast({
+     title,
+     duration,
+     icon:'none',
+   })
+}
+//导入网络请求模块
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,114 @@
 <template>
-  <view>
-    
+ <view>
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+  </view>
+  <view class="nav-list">
+     <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+       <image :src="item.image_src" class="nav-img"></image>
+     </view>
+  </view>
+  <view class="floor-list">
+    <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+      <image :src="item.floor_title.image_src" class="floor-title"></image>
+      <view class="floor-img-box">
+        <navigator class="left-img-box" :url="item.product_list[0].url">
+          <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+        </navigator>
+        <view class="right-img-box">
+          <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-show="i2 !== 0" :url="item2.url">
+            <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+          </navigator>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        
-      };
+<script setup>
+  import { ref,reactive } from 'vue';
+  import {onLoad } from '@dcloudio/uni-app';
+  const swiperList = ref([])
+  const navList = ref([])
+  const floorList = ref([])
+  onLoad(()=>{
+    getSwiperList()
+    getNavList()
+    getFloorList()
+   })
+  const getSwiperList = async () => {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+          if (res.meta.status !== 200) {
+             return uni.$showToast()
+           }
+          swiperList.value = res.message
+   }
+   const getNavList = async () => {
+       const { data: res } = await uni.$http.get('/api/public/v1/home/catitems')
+           if (res.meta.status !== 200) {
+              return uni.$showToast()
+            }
+           navList.value = res.message
     }
-  }
+    const getFloorList = async () => {
+        const { data: res } = await uni.$http.get('/api/public/v1/home/floordata')
+        if (res.meta.status !== 200) {
+            return uni.$showToast()
+            }
+            console.log(res.message)
+         res.message.forEach(floor => {
+            floor.product_list.forEach(prod => {
+              prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+            })
+          })    
+          floorList.value = res.message
+     }
+    function navClickHandler(item){
+      if(item.name === "分类"){
+        uni.switchTab({
+          url:"/pages//cate/cate"
+        })
+      }
+    }
 </script>
 
 <style lang="scss">
+swiper {
+ height: 330rpx;
+ .swiper-item,
+ image {
+   width: 100%;
+   height: 100%;
+ }
+}
+.nav-list {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px 0;
 
+  .nav-img {
+    width: 128rpx;
+    height: 140rpx;
+  }
+}
+.floor-title {
+  height: 60rpx;
+  width: 100%;
+  display: flex;
+}
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
+}
 </style>
